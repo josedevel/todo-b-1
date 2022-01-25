25ec486fd9f6b97f374cdda50fd3eb0c61132431
Jose Lizano <josedevel@gmail.com>
2022-01-25 05:44:55 -0600

    Adding TODO list sort methods.

--- src/reducers/todoList.js
+++ src/reducers/todoList.js
@@ -13,14 +13,55 @@ export default(state = {
           ...state,
           //todoLists: state.todoLists.concat(action.todoList),
           todoLists: state.todoLists.concat(action.todoList).slice().sort(function(a, b) {
-            console.log('test1');
-            if(a.time > b.time){
+            if(a.priority === 'high' && b.priority === 'high'){
+              return 0;
+            }
+            if(a.priority === 'high' && b.priority === 'medium'){
               return -1;
             }
-            if(b.time > a.time){
+            if(a.priority === 'high' && b.priority === 'low'){
+              return -1;
+            }
+            if(a.priority === 'medium' && b.priority === 'high'){
               return 1;
             }
-            return 0;
+            if(a.priority === 'medium' && b.priority === 'low'){
+              return -1;
+            }
+            if(a.priority === 'low' && b.priority === 'high'){
+              return 1;
+            }
+            if(a.priority === 'low' && b.priority === 'medium'){
+              return 1;
+            }
+
+
+            if(b.priority === 'high' && a.priority === 'medium'){
+              return 1;
+            }
+            if(b.priority === 'high' && a.priority === 'low'){
+              return 1;
+            }
+            if(b.priority === 'medium' && a.priority === 'high'){
+              return -1;
+            }
+            if(b.priority === 'medium' && a.priority === 'low'){
+              return -1;
+            }
+            if(b.priority === 'low' && a.priority === 'high'){
+              return 1;
+            }
+            if(b.priority === 'low' && a.priority === 'medium'){
+              return 1;
+            }
+
+
+            if(a.priority === 'medium' && b.priority === 'medium'){
+              return 0;
+            }
+            if(a.priority === 'low' && b.priority === 'low'){
+              return 0;
+            }
           })
         };
       case TODO_LIST_CHECK:
