8f428b02fcf384014cd9d3288fdee5adaa8ac631
Jose Lizano <josedevel@gmail.com>
2022-02-02 19:18:42 -0600

    Code Clean Up.

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore} from "redux";
+import {createStore, applyMiddleware} from "redux";
 import {Provider} from 'react-redux';
 import reducer from './reducer';
 import App from './App';
+import thunkMiddleware from 'redux-thunk';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 
 const store = createStore(
   reducer,
+  applyMiddleware(thunkMiddleware)
 );
 
 ReactDOM.render(
--- src/reducer.js
+++
@@ -1,6 +0,0 @@
-import todoList from "./reducers/todoList";
-import {combineReducers} from "redux";
-
-export default combineReducers({
-    todoList,
-});
