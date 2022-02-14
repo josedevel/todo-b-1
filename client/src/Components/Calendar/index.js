b1a36722697bfe9c53403de14436bad05b7e5a8f
Jose Lizano <josedevel@gmail.com>
2022-02-14 17:48:18 -0600

    Adding calendar logic. (Testing).

--- client/src/Components/Calendar/index.js
+++ client/src/Components/Calendar/index.js
@@ -2,19 +2,33 @@ import React from "react";
 
 
 const today = new Date();
+const firstOfTheMonth = new Date(today.getFullYear(), today.getMonth()+1, 1).getDay();
 const weekdaysShort = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 const lastDayOfCurrentMonth = new Date(today.getFullYear(), today.getMonth()+1, 0).getDate();
-const daysOfMonth = [
-    [1,2,3,4,5,6,7],
-    [8,9,10,11,12,13,14],
-    [15,16,17,18,19,20,21],
-    [22,23,24,25,26,27,28],
-    [29,30,31]
-]
+let daysOfMonth = [];
+
+const test = () => {
+    let j = 0;
+    let row = [];
+    let k = 0;
+    let p = 1;
+    for(let i=0; i<(lastDayOfCurrentMonth+firstOfTheMonth); i++){
+        row[k] = (i>=firstOfTheMonth)? p++ : '';
+        k++;
+        if(/*i>0 &&*/ k%7 === 0){
+            daysOfMonth[j] = row;
+            j++;
+            row = [];
+            k = 0;
+        }
+    }
+    daysOfMonth[j] = row;
+}
 
 const index = (props) => {
 
-  //console.log(daysOfMonth)
+  test();
+  console.log(firstOfTheMonth);
 
   return (
     <React.Fragment>
@@ -23,20 +37,20 @@ const index = (props) => {
         <thead>
           <tr>
             {weekdaysShort.map(day => {
-                return (
+              return (
                 <th>
-                    {day}
+                  {day}
                 </th>
-                )  
+              )  
             })}
           </tr>
         </thead>
         <tbody>
           {daysOfMonth.map(week => {
             return ( <tr>
-                {week.map(day => {
-                    return <td>{day}</td>
-                })}
+              {week.map(day => {
+                return <td>{day}</td>
+              })}
             </tr> )
           })}
         </tbody>
